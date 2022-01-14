0a3c4d936605fc181eb5a1bc2d08cd0f7f3f4086
Anna <annasarafanova91@gmail.com>
2022-01-13 23:10:48 -0800

    Work in progress

--- database_postgreSQL/index.js
+++ database_postgreSQL/index.js
@@ -1,14 +1,39 @@
-const postgres = require('postgres');
-const sql = postgres('postgres://annasarafanova:password@localhost:5432/sdc')
+const { Client, Pool } = require('pg');
 
-//test query
-const getTestAnswer = () => {
-  return (sql`select * from questions limit 1`)
+const client = new Client({
+  database: 'sdc',
+  port: 5432,
+});
+client.connect()
+  .then(() => { console.log('PostgreSQL connected!'); })
+  .catch(e => console.error(e.stack));
+
+module.exports.client = client;
+
+
+
+//Refactor: use pages, join answers
+//List Questions -> GET /qa/questions
+const getQuestionsList = (product_id, count, page) => {
+  return (client.query(`SELECT * FROM questions
+  WHERE product_id=${product_id}
+  ORDER BY helpful DESC
+  LIMIT ${count}
+  OFFSET ${page}
+  `))
 }
 
-//List Questions
-
+//Answers List -> GET /qa/questions/:question_id/answers
+ const getAnswersList = (question_id, page, count) => {
+  return (client.query(`SELECT *
+  FROM answers
+  WHERE question_id=${question_id}
+  ORDER BY helpful DESC
+  LIMIT ${count}
+  `))
+ }
 
 module.exports = {
-  getTestAnswer
+  getQuestionsList,
+  getAnswersList
 }
