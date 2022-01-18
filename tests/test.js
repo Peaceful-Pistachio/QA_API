2075e5080c8fa2c17caae9b62a987465a241e91e
Anna <annasarafanova91@gmail.com>
2022-01-17 17:29:07 -0800

    Suoer basic tests. making sure that framework works

--- tests/test.js
+++ tests/test.js
@@ -1,15 +1,49 @@
-const server = require('../server.js');
-let testServer = server.app.listen(3001);
-const supertest = require('supertest');
-const assert = require('assert');
-const express = require('express');
+// const server = require('../server.js');
+// const express = require('express');
+// const app = express();
+// const request = require('supertest');
 
-const request = supertest(testServer);
-const db = require('../model);
 
-describe('Basic testing of tests and server:', () => {
+// describe('Basic testing of tests and server:', () => {
+//   test("GET /", (done) => {
+//     request(app)
+//       .get('/qa/questions?product_id=70460&count=3&page=1')
+//       .expect(404)
+//       .end(done)
+//   });
+// })
 
-  it('Testing to see if Jest works', () => {
-    expect(1).toBe(1)
-  })
-})
+const frisby = require('frisby');
+const Joi = frisby.Joi;
+
+it ('GET questions should return a status of 200', function () {
+  return frisby
+    .get('http://localhost:3000/qa/questions?product_id=70460&count=3&page=1')
+    .expect('status', 200)
+});
+
+it ('GET answers should return a status of 200', function () {
+  return frisby
+    .get(`http://localhost:3000/qa/questions/247762/answers?count=2&page=1`)
+    .expect('status', 200);
+});
+
+it ('POST question should return a status of 201 Created', function () {
+  return frisby
+    .post('http://localhost:3000/qa/questions', {
+      body: {
+        "id": 3518967,
+        "product_id": 70460,
+        "body": "hello can you help me?",
+        "date_written": "0",
+        "asker_name": "avona",
+        "asker_email": "aaa@gmail.com",
+        "reported": false,
+        "helpful": 0,
+        "date": null,
+        "answers": []
+    }
+
+    })
+    .expect('status', 201);
+});
